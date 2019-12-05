025d40b92e5a700a33e34ac901481f22a81267c0
Elizabeth Smikle <smiklee@sheridancollege.ca>
2019-12-04 22:43:33 -0500

    Data access and business logic layers added

---
+++ LibraryManagement/Member/DataAccessLayer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagement.Member
+{
+    public class DataAccessLayer
+    {
+        private LibraryDBDataSetTableAdapters.BooksTableAdapter adpBooks;
+        private LibraryDBDataSet.BooksDataTable tblBooks;
+        private List<BusinessLogicLayer> lstBooks;
+
+        private LibraryDBDataSetTableAdapters.RequestsTableAdapter adpRequests;
+        private LibraryDBDataSet.RequestsDataTable tblRequests;
+        private List<BusinessLogicLayer> lstRequests;
+        
+        public DataAccessLayer()
+        {
+            adpBooks = new LibraryDBDataSetTableAdapters.BooksTableAdapter();
+            tblBooks = new LibraryDBDataSet.BooksDataTable();
+            lstBooks = null;
+
+            adpRequests = new LibraryDBDataSetTableAdapters.RequestsTableAdapter();
+            tblRequests = new LibraryDBDataSet.RequestsDataTable();
+            lstRequests = null;
+        }
+
+        public List<BusinessLogicLayer> GetBooksByRenterId(int renterId)
+        {
+            tblBooks = adpBooks.GetBooksByRenterId(renterId);
+
+            if(tblBooks.Count > 0)
+            {
+                lstBooks = new List<BusinessLogicLayer>();
+
+                foreach(var row in tblBooks)
+                {
+                    BusinessLogicLayer book = new BusinessLogicLayer();
+
+                    book.Id = row.BookId;
+                    book.Title = row.Title;
+                    book.Genre = row.Genre;
+                    book.Author = row.Author;
+                    book.DueDate = row.DueBy.ToString();
+
+                    lstBooks.Add(book);
+                }
+            }
+            return lstBooks;
+        }
+
+        public List<BusinessLogicLayer> GetRequestsById(int id)
+        {
+            tblRequests = adpRequests.GetRequestsByUserId(id);
+
+            if (tblRequests.Count > 0)
+            {
+                lstRequests = new List<BusinessLogicLayer>();
+
+                foreach (var row in tblRequests)
+                {
+                    BusinessLogicLayer request = new BusinessLogicLayer();
+
+                    request.Id = row.BookId;
+                    request.RequestType = row.RequestType;
+                    request.RequestStatus = row.RequestStatus;
+
+                    lstRequests.Add(request);
+                }
+            }
+            return lstRequests;
+        }
+    }
+}
---
+++ LibraryManagement/Member/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibraryManagement.Member
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        private BusinessLogicLayer bll;
+
+        public Home()
+        {
+            bll = new BusinessLogicLayer();
+        }
+
+        private void RefreshGridView()
+        {
+            int userId = 1;
+            List<BusinessLogicLayer> lstBooks = bll.GetBooksByRenterId(userId);
+            List<BusinessLogicLayer> lstRequests = bll.GetRequestsById(userId);
+
+            if(lstBooks != null)
+            {
+                grdBooks.DataSource = lstBooks;
+                grdBooks.DataBind();
+            }
+            else
+            {
+                lblBooksGrid.Text = "No books issued";
+                grdBooks.DataSource = null;
+                grdBooks.DataBind();
+            }
+
+            if (lstRequests != null)
+            {
+                grdRequests.DataSource = lstRequests;
+                grdRequests.DataBind();
+            }
+            else
+            {
+                lblRequestsGrid.Text = "No requests";
+                grdRequests.DataSource = null;
+                grdRequests.DataBind();
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                RefreshGridView();
+        }
+    }
+}
